2690d2ea9218ee7d2b1d6a5d637b1dd9f3d9c69c
mookkae <ehsk2315@kyungsung.ac.kr>
2026-02-19 17:34:57 +0900

    [chore] Spring Boot 프로젝트 초기화

---
+++ src/main/java/com/intime/IntimeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.intime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntimeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntimeBackendApplication.class, args);
+	}
+
+}
