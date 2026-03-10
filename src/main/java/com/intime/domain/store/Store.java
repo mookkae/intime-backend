d8ba8a71612bef1ce8aecaed1ad526eb3efa6b3d
mookkae <ehsk2315@kyungsung.ac.kr>
2026-03-11 03:12:11 +0900

    [feat] Store 도메인 레이어 구성
    Related #5

---
+++ src/main/java/com/intime/domain/store/Store.java
@@ -0,0 +1,43 @@
+package com.intime.domain.store;
+
+import com.intime.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "store")
+public class Store extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private int estimatedWaitMinutes;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Store(String name, String address, int estimatedWaitMinutes) {
+        this.name = name;
+        this.address = address;
+        this.estimatedWaitMinutes = estimatedWaitMinutes;
+    }
+
+    public static Store create(String name, String address, int estimatedWaitMinutes) {
+        return Store.builder()
+                .name(name)
+                .address(address)
+                .estimatedWaitMinutes(estimatedWaitMinutes)
+                .build();
+    }
+}
