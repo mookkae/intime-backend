24ddad3b7a93415cc0c1e92507cd7de535dcfaec
mookkae <ehsk2315@kyungsung.ac.kr>
2026-03-16 14:01:43 +0900

    [chore] websocket, spring-retry, spring-aspects 의존성 추가
    Related #24

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,9 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("org.springframework.retry:spring-retry:2.0.11")
+    implementation("org.springframework:spring-aspects")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.projectlombok:lombok")
